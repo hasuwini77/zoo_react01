c4e2bd6469bf30a8880d36827e6c6cce1a4599b5
Edwin Andermyr <andermyredwin@gmail.com>
2024-03-31 17:44:39 +0200

    added additional css fix

--- src/components/AnimalModal/index.jsx
+++ src/components/AnimalModal/index.jsx
@@ -16,7 +16,11 @@ const AnimalModal = ({ currentAnimal, onClose }) => {
         <img className={styles.contentImg} src={getImageURL(currentAnimal.smallimg)} alt={currentAnimal.img} onError={handleImageError} />
         <h1 className={styles.title}>{currentAnimal.name}</h1>
         <p className={styles.description}>{currentAnimal.description}</p>
+        <p className={styles.lifespan}> This Animal typically lives: {currentAnimal.lifespan}</p>
         <p className={styles.food}>Usually eats: {currentAnimal.food}</p>
+        <p className={styles.length}>Length: {currentAnimal.length}</p>
+        <p className={styles.weight}>Weight: {currentAnimal.weight}</p>
+        <p className={styles.location}>Where can you find me? in {currentAnimal.location}</p>
         <button className={styles.closeButton2} onClick={onClose}>
           Go Back
         </button>
